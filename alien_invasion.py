78fc52ffac899cffb025409e12e415fb3427bd9e
李西峰 <lixifah@163.com>
2022-04-05 18:38:43 +0800

    更新屏幕作为一个方法，供调用

--- alien_invasion.py
+++ alien_invasion.py
@@ -20,12 +20,7 @@ def run_game():
     while True:
         gf.check_event()
 
-        # 每次循环时都重绘屏幕
-        screen.fill(ai_settings.bg_color)
-        ship.blitme()
-
-        # 让最近绘制的屏幕可见
-        pygame.display.flip()
+        gf.update_screen(ai_settings, screen, ship)
 
 
 run_game()
--- game_functions.py
+++ game_functions.py
@@ -1,3 +1,4 @@
+# -*- coding: UTF-8 -*-
 
 import sys
 import pygame
@@ -10,3 +11,11 @@ def check_event():
             sys.exit()
 
 
+def update_screen(ai_settings, screen, ship):
+    """更新屏幕上的图像，并切换到新屏幕"""
+    # 每次循环时都重绘屏幕
+    screen.fill(ai_settings.bg_color)
+    ship.blitme()
+
+    # 让最近绘制的屏幕可见
+    pygame.display.flip()
