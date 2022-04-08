e3b4b82b5eda5856249c5f76b568091d76ee1379
李西峰 <lixifah@163.com>
2022-04-08 21:46:00 +0800

    重构check_event函数，拆分为check_keydown_event、vcheck_keyup_event两个函数

--- game_functions.py
+++ game_functions.py
@@ -4,6 +4,22 @@ import sys
 import pygame
 
 
+def check_keydown_event(event, ship):
+    """响应按键"""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_right = False
+
+
+def check_keyup_event(event, ship):
+    """响应松开"""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_left = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
+
+
 def check_event(ship):
     """响应按键和鼠标事件"""
     for event in pygame.event.get():
@@ -11,16 +27,10 @@ def check_event(ship):
             sys.exit()
 
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = True
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = True
+            check_keydown_event(event, ship)
 
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = False
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = False
+            check_keyup_event(event, ship)
 
 
 def update_screen(ai_settings, screen, ship):
