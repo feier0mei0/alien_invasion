383a2ccbad96cfb57f4285c7202cd5d7322dc71e
李西峰 <lixifah@163.com>
2022-04-09 19:12:48 +0800

    限制子弹个数

--- game_functions.py
+++ game_functions.py
@@ -6,7 +6,7 @@ import pygame
 from bullet import Bullet
 
 
-def check_keydown_event(event, ai_settings, screen, ship, bullets):
+def check_keydown_events(event, ai_settings, screen, ship, bullets):
     """响应按键"""
     if event.key == pygame.K_RIGHT:
         ship.moving_right = True
@@ -14,11 +14,12 @@ def check_keydown_event(event, ai_settings, screen, ship, bullets):
         ship.moving_left = True
     elif event.key == pygame.K_SPACE:
         # 创建一颗子弹，并将其加入到编组bullets中
-        new_bullet = Bullet(ai_settings, screen, ship)
-        bullets.add(new_bullet)
+        if len(bullets) < ai_settings.bullet_allowed:
+            new_bullet = Bullet(ai_settings, screen, ship)
+            bullets.add(new_bullet)
 
 
-def check_keyup_event(event, ship):
+def check_keyup_events(event, ship):
     """响应松开"""
     if event.key == pygame.K_RIGHT:
         ship.moving_right = False
@@ -33,10 +34,10 @@ def check_event(ai_settings, screen, ship, bullets):
             sys.exit()
 
         elif event.type == pygame.KEYDOWN:
-            check_keydown_event(event, ai_settings, screen, ship, bullets)
+            check_keydown_events(event, ai_settings, screen, ship, bullets)
 
         elif event.type == pygame.KEYUP:
-            check_keyup_event(event, ship)
+            check_keyup_events(event, ship)
 
 
 def update_screen(ai_settings, screen, ship, bullets):
