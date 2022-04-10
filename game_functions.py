6914d0a341178ee28839e9807c4ef59e5aa3831a
lixifeng <lixifah@163.com>
2022-04-10 20:25:14 +0800

    提高子弹速度，抽取公共方法：check_bullet_alien_collision，清空子弹及再新建一群外星人

--- game_functions.py
+++ game_functions.py
@@ -75,8 +75,12 @@ def update_bullets(ai_settings, screen, ship, aliens, bullets):
     for bullet in bullets.copy():
         if bullet.rect.bottom <= 0:
             bullets.remove(bullet)
-    # 检查是否有子弹击中了外星人
-    # 如果是这样，就删除相应子弹和外星人
+    check_bullet_alien_collision(ai_settings, screen, ship, aliens, bullets)
+
+
+def check_bullet_alien_collision(ai_settings, screen, ship, aliens, bullets):
+    """响应子弹和外星人的碰撞"""
+    # 删除发生碰撞的子弹和外星人
     collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
 
     if len(aliens) == 0:
