fd2d36e32841cb0acc0bf0d927797c7589873b11
李西峰 <lixifah@163.com>
2022-04-10 15:36:05 +0800

    抽取公共方法：获取一行可容纳多少外星人get_number_aliens_x、创建外星人create_alien

--- game_functions.py
+++ game_functions.py
@@ -79,18 +79,27 @@ def update(bullets):
 
 
 def create_fleet(ai_settings, screen, aliens):
-    """创建外星人"""
-    # 创建一个外星人，并计算一行可容纳多少个外星人
-    # 外星人间距为外星人宽度
+    """创建外星人群"""
     alien = Alien(ai_settings, screen)
     alien_width = alien.rect.width
-    available_space_x = ai_settings.screen_width - 2 * alien_width
-    number_aliens_x = available_space_x / (2*alien_width)
+    number_aliens_x = get_number_aliens_x(ai_settings, alien_width)
 
     # 创建第一行外星人
     for alien_number in range(number_aliens_x):
-        # 创建一个外星人并将其加入当前行
-        alien = Alien(ai_settings, screen)
-        alien.x = alien_width + 2 * alien_width* alien_number
-        alien.rect.x = alien.x
-        aliens.add(alien)
+        create_alien(ai_settings, screen, aliens, alien_number)
+
+
+def get_number_aliens_x(ai_settings, alien_width):
+    """计算一行可容纳多少外星人"""
+    available_space_x = ai_settings.screen_width - 2 * alien_width
+    number_aliens_x = int(available_space_x / (2 * alien_width))
+    return number_aliens_x
+
+
+def create_alien(ai_settings, screen, aliens, alien_number):
+    """创建一个外星人并将其加入当前行"""
+    alien = Alien(ai_settings, screen)
+    alien_width = alien.rect.width
+    alien.x = alien_width + 2 * alien_width * alien_number
+    alien.rect.x = alien.x
+    aliens.add(alien)
