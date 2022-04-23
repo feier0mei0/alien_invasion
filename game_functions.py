649b079174aef2c59469904c94a0e7c9066af124
lixifeng <lixifah@163.com>
2022-04-23 15:05:06 +0800

    显示游戏得分

--- game_functions.py
+++ game_functions.py
@@ -76,7 +76,7 @@ def check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bul
         ship.center_ship()
 
 
-def update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button):
+def update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets, play_button):
     """更新屏幕上的图像，并切换到新屏幕"""
     # 每次循环时都重绘屏幕
     screen.fill(ai_settings.bg_color)
@@ -86,6 +86,9 @@ def update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button
     # 在飞船和外星人后面重绘所有子弹
     for bullet in bullets:
         bullet.draw_bullet()
+
+    # 显示得分
+    sb.show_score()
 
     # 如果游戏处于非活动状态，就绘制Play按钮
     if not stats.game_active:
