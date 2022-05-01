633f00e427cf28a5bbbb8e6988e29955bd36f02f
lixifeng <lixifah@163.com>
2022-05-01 17:39:56 +0800

    调整图表尺寸以适应屏幕

--- matplotlib/rw_visual.py
+++ matplotlib/rw_visual.py
@@ -10,12 +10,15 @@ while True:
     rw = RandomWalk(50000)
     rw.fill_walk()
 
+    # 设置绘图窗口尺寸
+    plt.figure(dpi=128, figsize=(10, 6))
+
     point_numbers = list(range(rw.num_points))
     plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=15)
 
     # 突出随机漫步的起点和终点
     plt.scatter(0, 0, c='green', edgecolors='none', s=100)
-    plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=1)
+    plt.scatter(rw.x_values[-1], rw.y_values[-1], c='green', edgecolors='none', s=1)
 
     # 隐藏坐标轴
     plt.axes().get_xaxis().set_visible(False)
