f72d3b27e929a3ee867281b2efbe9818713d4446
lixifeng <lixifah@163.com>
2022-05-01 16:19:43 +0800

    使用颜色映射Blues着色随机漫步图

--- matplotlib/rw_visual.py
+++ matplotlib/rw_visual.py
@@ -7,9 +7,11 @@ from random_walk import RandomWalk
 # 只要程序处于活跃状态，就不断模拟随机漫步
 while True:
     # 创建一个RandomWalk实例，并将其包含的点都绘制出来
-    rm = RandomWalk()
-    rm.fill_walk()
-    plt.scatter(rm.x_values, rm.y_values, s=15)
+    rw = RandomWalk()
+    rw.fill_walk()
+
+    point_numbers = list(range(rw.num_points))
+    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolor='none', s=15)
     plt.show()
 
     keep_running = raw_input("Make another walk?(y/n): ")
