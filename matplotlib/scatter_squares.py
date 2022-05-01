50174ee44cf7bec8ce85373ef16cb592db9e10d3
lixifeng <lixifah@163.com>
2022-05-01 11:10:39 +0800

    scatter绘制单个点

---
+++ matplotlib/scatter_squares.py
@@ -0,0 +1,16 @@
+# -*- coding: UTF-8 -*-
+
+import matplotlib.pyplot as plt
+
+plt.scatter(2, 300, s=200)
+
+# 设置图表标题并给坐标轴加标签
+plt.title("Squares Numbers1", fontsize=24)
+plt.xlabel("Value", fontsize=14)
+plt.ylabel("Squares of value", fontsize=14)
+
+# 设置刻度标记的大小
+plt.tick_params(axis='both', which='major', labelsize=14)
+
+plt.show()
+
