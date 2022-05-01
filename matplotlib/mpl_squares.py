7a21771f21f3f5f6cca26ea0194c7f5abc0a4f53
lixifeng <lixifah@163.com>
2022-05-01 10:32:30 +0800

    绘制图表：增加标题、xy轴标签文字

--- matplotlib/mpl_squares.py
+++ matplotlib/mpl_squares.py
@@ -1,5 +1,13 @@
+# -*- coding: UTF-8 -*-
+
 import matplotlib.pyplot as plt
 
 squares = [1, 4, 9, 16, 25]
-plt.plot(squares)
+plt.plot(squares, linewidth=5)
+
+# 设置图表标题，并给坐标轴加标签
+plt.title("Square Numbers", fontsize=24)
+plt.xlabel("Value", fontsize=14)
+plt.ylabel("Square of value", fontsize=14)
+
 plt.show()
