dbda8bf5f07a5724318cd026b4798c0df404fce3
lixifeng <lixifah@163.com>
2022-05-01 11:29:37 +0800

    使用scatter绘制1000个点

--- matplotlib/scatter_squares.py
+++ matplotlib/scatter_squares.py
@@ -2,10 +2,10 @@
 
 import matplotlib.pyplot as plt
 
-x_values = [1, 2, 3, 4, 5]
-y_values = [1, 4, 9, 16, 25]
+x_values = list(range(1, 1001))
+y_values = [x**2 for x in x_values]
 
-plt.scatter(x_values, y_values, s=100)
+plt.scatter(x_values, y_values, s=40)
 
 # 设置图表标题并给坐标轴加标签
 plt.title("Squares Numbers1", fontsize=24)
@@ -14,6 +14,9 @@ plt.ylabel("Squares of value", fontsize=14)
 
 # 设置刻度标记的大小
 plt.tick_params(axis='both', which='major', labelsize=14)
+
+# 设置每个坐标轴的取值范围
+plt.axis([0, 1100, 0, 1100000])
 
 plt.show()
 
